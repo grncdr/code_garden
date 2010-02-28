9321b0f95e2fd92adbcd0b08d73c890dc7edc2b3
Stephen Sugden <grncdr@uvic.ca>
2010-02-27 21:56:20 -0800

    maybe getting closer to something visible?

--- code_garden.js
+++ code_garden.js
@@ -1,16 +1,57 @@
 Garden = {
 	width: function() { return 500 }, 
 	height: function() { return 600 },
+	repo: undefined,
 	drawables: [],
 	b_counts: [],
 	f_counts: [],
 	l_counts: [],
 	splitRadius: function() { return 8 },
 
+	grow: function(url){
+		var G = this
+		jQuery.getJSON('http://github.com/api/v2/json/repos/show/'+url+'/branches', null, function(refs){
+			G.refs = refs
+			if(typeof refs.branches.master == 'undefined'){
+				for(var branch in refs.branches){
+					this.current_ref = refs.branches[branch] // Just return the first branch
+				}
+			} else { 
+				this.current_ref = refs.branches.master
+			}
+			jQuery.getJSON('http://github.com/api/v2/json/blobs/all/'+url+'/'+this.current_ref, null, function(d){
+				for(blob in d.blobs){
+					pathname = blob.split('/')
+					if(pathname.length > 1){
+						var current_root = G.Ground.addOrGetSplit(pathname[0])
+						for(var i=1; i<pathname.length-1; i++){
+							current_root = current_root.addOrGetSplit(pathname[i])
+						}
+					} else {
+						current_root = G.Ground
+					}
+					current_root.addBlob(d.blobs[blob])
+				}
+			})
+		})
+	},
+
+	hasItem: function(sha1, level){
+		if(typeof level != 'undefined'){
+			return this.drawables[level].filter(function(item){return item.sha1 == sha1}).length > 0
+		}
+		for(var level=0; level<this.drawables.length; level++){
+			if(this.drawables[level].filter(function(item){return item.sha1 == sha1}).length > 0){
+				return true;
+			}
+		}
+		return false;
+	},
+
 	addItem: function(item, level){
 		var drawables = this.drawables[level]
 		drawables.push(item)
-		if(item instanceof Flower){
+		if(typeof item.lineCount != 'undefined'){
 			this.increment(this.f_counts, level)
 			this.increment(this.l_counts, level, item.lineCount())
 		} else {
@@ -18,20 +59,20 @@ Garden = {
 		}
 	},
 
-	'increment': function(arr, level, amount){
+	increment: function(arr, level, amount){
 		if(typeof amount == 'undefined'){
 			amount = 1
 		}
 		arr[level] = arr[level] + amount
 	},
 
-	'intersecting': function(){
+	intersecting: function(){
 		var x = p.mouseX
 		var y = p.mouseY
 		return null
 	},
 
-	'getX': function(item){
+	getX: function(item){
 		var xpos = 0
 		var level = 0
 		for(var level=0; level < this.drawables.length; level++){
@@ -45,15 +86,15 @@ Garden = {
 		}
 	},
 
-	'getY': function(level){
+	getY: function(level){
 		if(level == 0){
 			return this.height()-1
 		}
 		return this.height() - (this.height() / this.drawables.length) * level
 	},
 
-	'getRadius': function(item, level){
-		if(item instanceof Root){
+	getRadius: function(item, level){
+		if(typeof item.lineCount == 'undefined'){
 			return this.splitRadius
 		} else {
 			var avgLines = this.lineCount(level)/ this.f_counts[level]
@@ -65,7 +106,7 @@ Garden = {
 		}
 	},
 
-	'flowerSpace': function(level){
+	flowerSpace: function(level){
 		return this.width() - this.splitRadius * 2 * this.b_counts[level]
 	},
 }
@@ -127,10 +168,10 @@ Garden.Stem = { // implements Drawable
 		}
 		start = this.root.center()
 		ydiff = start.y-end.y
-		noFill()
-		strokeWeight(2)
-		stroke(10, 100, 10)
-		bezier( start.x, start.y, 
+		p.noFill()
+		p.strokeWeight(2)
+		p.stroke(10, 100, 10)
+		p.bezier( start.x, start.y, 
 						start.x, start.y-ydiff*0.4, 
 						end.x,   end.y+ydiff*0.7, 
 						end.x,   end.y)
@@ -145,11 +186,35 @@ Garden.Root = { // implements Drawable
 	_center: null, // Point
 	level: 0,
 
+	addOrGetSplit: function(dirname){
+		var substems = stems.filter(function(stem){
+			return stem.destination.sha1 == 'undefined' && stem.destination.name == dirname
+		})
+		if(substems.length > 0){
+			return substems[0]
+		}
+		// subdirectory has not been added yet, create it and return it
+		var newSplit = Object.beget(Garden.StemSplit, {
+			name: dirname
+		})
+		this.addItem(newSplit)
+		return newSplit
+	},
+
+	addBlob: function(sha1){
+		newFlower = Object.create(Garden.Flower, {
+			sha1: sha1
+		})
+		this.addItem(newFlower)
+	},
+
 	addItem: function(onStem){
-		onStem.root = this
+		stems.push( Object.beget(Garden.Stem, {
+			root: this,
+			destination: onStem,
+		}))
 		onStem.level = this.level + 1
-		Gardener.addItem(onStem, level+1)
-		stems.push( Object.create(Garden.Stem, {'root':this, 'destination':onStem}) )
+		Garden.addItem(onStem, this.level+1)
 	},
 
 	center: function(){
@@ -170,25 +235,24 @@ Garden.Root = { // implements Drawable
 	},
 }
 
-Garden.Ground = Object.create(Garden.Root, {
+Garden.Ground = Object.beget( Garden.Root, {
 	roots: [],
 
-	addItem: function(flower){
-		root = Object.create(Garden.Root)
+	addItem: function(onStem){
+		root = Object.beget(Garden.Root)
 		Gardener.addItem(root, 0)
-		root.addItem(flower)
-		roots.push(root)
+		root.addItem(onStem)
+		this.roots.push(root)
 	},
 
 	draw: function(){
-		for(i=0; i<roots.length; i++){
+		for(i=0; i<this.roots.length; i++){
 			this.roots[i].draw()
 		}
 	}
 })
 
-Garden.StemSplit = Object.create(Garden.Root, { // implements OnStem, Hoverable 
-	base: null,
+Garden.StemSplit = Object.beget(Garden.Root, { // implements OnStem, Hoverable 
 	dirname: "",
 
 	intersecting: function(x, y){
@@ -211,7 +275,7 @@ Garden.StemSplit = Object.create(Garden.Root, { // implements OnStem, Hoverable
 })
 
 
-Garden.LineBase = Object.create(Object, { //implements Lines 
+Garden.LineBase = Object.beget(Object, { //implements Lines 
 	children: [],
 	_center: null, // Point
 	_radius: -1,
@@ -225,17 +289,16 @@ Garden.LineBase = Object.create(Object, { //implements Lines
 		return count
 	},
 
-	'addLine': function(line){
+	addLine: function(line){
 		line.owner = this
 		this.children.push(line)
 	},
 
 })
 
-Garden.Flower = Object.create(Garden.LineBase, { // implements OnStem, Drawable, Petal, Hoverable
+Garden.Flower = Object.beget(Garden.LineBase, { // implements OnStem, Drawable, Petal, Hoverable
 	filename: null,
 	line_count: 0,
-	base: null,
 
 	// Deprecated
 	getFlower: function(){
@@ -255,24 +318,24 @@ Garden.Flower = Object.create(Garden.LineBase, { // implements OnStem, Drawable,
 
 	radius: function(){
 		if(_radius < 0){
-			_radius = sqrt(pow(maxRadius(),2.) * (children.length/lineCount()))
+			this._radius = sqrt(pow(maxRadius(),2.) * (children.length/lineCount()))
 		}
-		return _radius
+		return this._radius
 	},
 
 	maxRadius: function(){
 		if(_maxRadius < 0){
-			_maxRadius= Gardener.getRadius(this, level)
+			this._maxRadius= Gardener.getRadius(this, level)
 		}
-		return _maxRadius
+		return this._maxRadius
 	},
 
 	center: function(){
-		if(_center.x == -1 && _center.y == -1){
-			_center.x = Gardener.getX(this)
-			_center.y = Gardener.getY(this.level)
+		if(this._center.x == -1 && this._center.y == -1){
+			this._center.x = Gardener.getX(this)
+			this._center.y = Gardener.getY(this.level)
 		}
-		return _center
+		return this._center
 	},
 
 	draw: function() {
@@ -282,10 +345,10 @@ Garden.Flower = Object.create(Garden.LineBase, { // implements OnStem, Drawable,
 		flower_layer.strokeWeight(2)
 		flower_layer.fill(220, 220, 0)
 		flower_layer.stroke(180, 180, 0)
-		flower_layer.ellipse(_center.x, _center.y, radius()*2, radius()*2)
+		flower_layer.ellipse(_center.x,	this._center.y, radius()*2, radius()*2)
 		noFill()
 		stroke(255,200,200)
-		ellipse(_center.x, _center.y, maxRadius()*2, maxRadius()*2)
+		ellipse(_center.x,	this._center.y, maxRadius()*2, maxRadius()*2)
 	},
 
 	intersecting: function(x, y){
@@ -302,7 +365,7 @@ Garden.Flower = Object.create(Garden.LineBase, { // implements OnStem, Drawable,
 	},
 })
 
-Garden.Line = Object.create(Garden.LineBase, { // implements Drawable, Petal, Hoverable
+Garden.Line = Object.beget(Garden.LineBase, { // implements Drawable, Petal, Hoverable
 	pos: 0,
 	indent: 0,
 	_angle: -1,
@@ -357,17 +420,17 @@ Garden.Line = Object.create(Garden.LineBase, { // implements Drawable, Petal, Ho
 	radius: function(){
 		if (_radius < 0){
 			proportion = lineCount() / owner.lineCount()
-			_radius = sqrt(proportion * pow( owner.radius() ,2. ))
+			this._radius = sqrt(proportion * pow( owner.radius() ,2. ))
 		}
-		return _radius
+		return this._radius
 	},
 
 	angle: function(){
 		if(_angle < 0){
 			index = owner.children.indexOf(this)
-			_angle = 2 * PI * (index / owner.children.length)
+			this._angle = 2 * PI * (index / owner.children.length)
 		}
-		return _angle
+		return this._angle
 	},
 
 	center: function(){
@@ -379,7 +442,7 @@ Garden.Line = Object.create(Garden.LineBase, { // implements Drawable, Petal, Ho
 			this._center.x = x
 			this._center.y = y
 		}
-		return _center
+		return this._center
 	},
 
 	draw: function() {
@@ -413,3 +476,31 @@ Garden.Line = Object.create(Garden.LineBase, { // implements Drawable, Petal, Ho
 		return null
 	},
 })
+
+jQuery(function(){
+	var sk = Processing($('canvas#garden')[0])
+	Garden.grow('grncdr/rtorrent-client')
+  var flower_layer = sk.createGraphics(Garden.width(), Garden.height(), sk.JAVA2D);
+	
+	// This was setup
+  sk.size(Garden.width(), Garden.height());
+  sk.stroke(125);
+  sk.fill(253, 162, 110);  
+	
+  //coordsText = "Flower width " + Garden.flowerSpace(1) +", "+ Garden.flowerSpace(2);
+  //print(Garden.asString());
+
+  sk.frameRate(30);
+	sk.draw = function() {
+		this.background(200, 200, 255);
+		this.smooth();
+		flower_layer.beginDraw();
+		flower_layer.smooth();
+		Garden.ground.draw();
+		flower_layer.endDraw();
+		sk.image(flower_layer, 0, 0);
+	}
+
+	sk.loop()
+})
+
